a6eb3273e7da4e686626aa72b7b9dcd974b69722
SalvatoreDuke <salvatore@estudante.ufscar.br>
2026-04-26 13:38:26 -0300

    Livraria commit 1

---
+++ src/main/java/br/ufscar/dc/dsw/dao/br/ufscar/dc/dsw/dao/GenericDAO.java
@@ -0,0 +1,29 @@
+package br.ufscar.dc.dsw.dao;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+abstract public class GenericDAO {
+
+    public GenericDAO() {
+        try {
+            /* Setup Banco de dados Derby */
+            // Class.forName("org.apache.derby.jdbc.ClientDriver");
+
+            /* Setup Banco de dados MySQL */
+            Class.forName("com.mysql.cj.jdbc.Driver");
+        } catch (ClassNotFoundException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    protected Connection getConnection() throws SQLException {
+
+
+        String url = "jdbc:mysql://localhost:3306/dsw1_bd";
+
+
+        return DriverManager.getConnection(url, "root", "root");
+    }
+}
